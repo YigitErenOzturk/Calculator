a9db867e7910099f4d9b89a1a4564dc06e223bf1
oztur <oztur@YIIT>
2022-11-22 12:03:54 +0100

    Proje dosyası ekle.

---
+++ ders2/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Welcome To Calculator");
+Console.WriteLine("Enter First Number");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Second Number");
+int second = int.Parse(Console.ReadLine());
+Console.WriteLine("Sonuç");
+Console.WriteLine(first + "+" + second + " = " + (first + second));
+Console.WriteLine(first + "-" + second + " = " + (first - second));
+Console.WriteLine(first + "/" + second + " = " + (first / second));
+Console.WriteLine(first + "X" + second + " = " + (first * second));
+
+Console.WriteLine("Welcome To Calculator");
+Console.WriteLine("Enter First Number");
+int first = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter Second Number");
+int second = int.Parse(Console.ReadLine());
+Console.WriteLine("Choose Process (+ - / *)");
+string third = Console.ReadLine();
+
+if (third == "+")
+{
+    Console.WriteLine(first + second);
+}
+if (third == "-") 
+{
+    Console.WriteLine(first - second);
+}
+if (third == "/")
+{
+    Console.WriteLine(first / second);
+}
+if (third == "*")
+{
+    Console.WriteLine(first * second);
+}
